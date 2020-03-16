272d18d8889b86c05a1da99d6779a519bfde7e74
MishaMiron <53948817+MykhailoEvtukh@users.noreply.github.com>
2020-03-16 11:50:45 +0200

    last commit

---
+++ src/food.js
@@ -0,0 +1,32 @@
+import dishesArr from './dishesArray.json';
+import dish from './food.hbs';
+
+const markDishes = dish(dishesArr);
+const menu = document.querySelector('#menu');
+const body = document.querySelector('body');
+
+const input = document.querySelector('.switch__input');
+input.addEventListener('input', changeTheme);
+
+let currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark-theme') {
+  body.classList.add(currentTheme);
+  input.checked = true;
+}
+
+function changeTheme() {
+  if (input.checked) {
+    body.classList.add('dark-theme');
+    localStorage.setItem('theme', 'dark-theme');
+  } else {
+    body.classList.toggle('dark-theme');
+    localStorage.setItem('theme', 'light-theme');
+  }
+}
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+menu.insertAdjacentHTML('afterbegin', markDishes);
